c9ac9467df937b079774eec6cd97e5b20e644763
Anna <anna.kirilicheva91@gmail.com>
2023-01-28 20:12:33 +0300

    Добавили плавный скролл

--- js/animation.js
+++ js/animation.js
@@ -20,3 +20,40 @@ function initIncreaseNumberAnimation () {
     increaseNumerAnimationStep (0,element,5000);
 };
 initIncreaseNumberAnimation();
+
+document.querySelector('#budget').addEventListener('change',
+function handleSelectChange(event) {
+    if(event.target.value === 'other') {
+        const formContainer = document.createElement('div');
+        formContainer.classList.add('form__group');
+        formContainer.classList.add('form__other-input');
+
+        const input = document.createElement('input');
+        input.placeholder =  'Введите ваш вариант';
+        input.type =  'text';
+        formContainer.appendChild(input);
+        document.querySelector('.form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+    }
+    const otherInput = document.querySelector('.form__other-input');
+    if(event.target.value !== 'other' && Boolean(otherInput)) {
+        document.querySelector('.form form').removeChild(otherInput);
+    }
+}
+);
+
+
+function addSmoothScroll(anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+ 
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+}
+ 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  addSmoothScroll(anchor);
+});
+
+addSmoothScroll(document.querySelector('.more-button'));
